afea77207ddd019d361319f67f5114a5f4607b06
CraftyDH <Heraldda@bcc.act.edu.au>
2017-10-07 16:35:46 +1100

    Put $core on top instead of ajax

--- darkLib.js
+++ darkLib.js
@@ -1,3 +1,22 @@
+var $DL = {};
+(function () {
+    $DL.require = function (name, type, url) {
+        if (name === undefined) {
+            return "Location is required";
+        }
+        if (type === "DLe") {
+            if (url === "DLe") {
+                var data = $DL.ajax.setJS("https://dark-ice-tech.github.io/DarkLib/extensions/" + name + ".min.js");
+            } else {
+                var data = $DL.ajax.setJS(url)
+            }
+        } else if(type === "js"){
+            var data = $DL.ajax(url);
+            eval(data);
+        }
+    };
+})();
+
 (function () {
     $DL.ajax = {};
 
@@ -21,23 +40,4 @@
             $('head').append('<script>' + js + '</script>');
         };
     }
-})();
-
-var $DL = {};
-(function () {
-    $DL.require = function (name, type, url) {
-        if (name === undefined) {
-            return "Location is required";
-        }
-        if (type === "DLe") {
-            if (url === "DLe") {
-                var data = $DL.ajax.setJS("https://dark-ice-tech.github.io/DarkLib/extensions/" + name + ".min.js");
-            } else {
-                var data = $DL.ajax.setJS(url)
-            }
-        } else if(type === "js"){
-            var data = $DL.ajax(url);
-            eval(data);
-        }
-    };
 })();
